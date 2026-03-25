b482a5cf71ea692e24c19080cf7da02aeeaf57a5
dzmagiorga <[200~184104905+dzmagiorga@users.noreply.github.com~>
2026-03-25 21:38:12 +0400

    Make functions add, subtract, multiply and divide

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// make functions: add, subtract, multiply, divide
+// we need three variables, 2 numbers and 1 operator
+// make a function named operate which takes therese 3 vars and applies one of the four functions
+
+const add = function(x, y) {
+    return x + y;
+}
+
+const subtract = function(x, y){
+    return x - y;
+}
+
+const multiply = function(x, y){
+    return x * y;
+}
+
+const divide = function(x, y){
+    return x / y;
+}
+
