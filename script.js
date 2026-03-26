443031c598a5574b5009203355ec877ba236edf8
dzmagiorga <[200~184104905+dzmagiorga@users.noreply.github.com~>
2026-03-26 15:04:20 +0400

    Partially write logic for inputting numbers

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let x, y = 0;
-let operator = "";
+let display = document.querySelector("#display");
+let buttons = document.querySelector("#buttons");
 
-const add = (x, y) => x + y;
+let x, y, operator;
+x = y = operator = "";
+display.textContent = "";
 
-const subtract = (x, y) => x - y;
+const add = (x, y) => Number(x) + Number(y);
 
-const multiply = (x, y) => x * y;
+const subtract = (x, y) => Number(x) - Number(y);
 
-const divide = (x, y) => x / y;
+const multiply = (x, y) => Number(x) * Number(y);
+
+const divide = (x, y) => Number(x) / Number(y);
 
 const operate = function(x, y, operator){
     switch(operator){
@@ -23,3 +27,20 @@ const operate = function(x, y, operator){
             return "Invalid operator";
     }
 }
+
+buttons.addEventListener("click", (event) => {
+    const isButton = event.target.classList.contains("Button");
+    if (isButton){
+        const buttonValue = event.target.textContent;
+        if (buttonValue.match(/[0-9]/g)){
+            if(operator === ""){
+                x += buttonValue;
+                display.textContent += buttonValue;
+            }
+            y += buttonValue;
+            display.textContent += buttonValue;
+        }
+        
+    }
+})
+
